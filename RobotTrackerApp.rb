f9ea922b385b9c376e0935ba278d5b1914c140a9
Karthik <eyank1195@gmail.com>
2019-10-07 20:45:19 +1100

    final commit with debugging puts removed and tested

--- RobotTrackerApp.rb
+++ RobotTrackerApp.rb
@@ -8,12 +8,13 @@ LEFT = "L"
 Setting the initial (x,y) coordinates of the Robot to (0,0)
 and the current(initial) direction to 0 (facing positive y-axis)
 =end
-x,y = 0
-current_direction = 0
+$x = 0
+$y = 0
+$current_direction = 0
 
 #General instructions to users about the Robots Behaviour
 def instructions
-    puts "\n\t\t<------Instructions------>"
+    puts "\n\t\t   <------Instructions------>"
     puts "Control the robot's behaviour with a series of `<command><number>` pair"
     puts "Use comma(,) to separate each pairs"
     puts "The robot accepts the following commands:"
@@ -32,87 +33,98 @@ def command_handler(commands)
     list = commands.split(",")
     list.each do |command|          
         if !(/^([#{FORWARD}#{BACKWARD}#{RIGHT}#{LEFT}])([0-9]+)$/.match?(command.upcase)) 
-            puts "Invalid input format please check the instructions"
-            break 
+            puts "\nInvalid input command please check the instructions if you are unsure"
+            return 
         else
             direction = command[0].upcase   
-            unit = command[1, command.length-1]   
+            unit = command[1, command.length-1].to_i  
             if /^[#{FORWARD}#{BACKWARD}]$/.match?(direction)
                 move_robot(direction, unit)  
             else
-                rotate_robot(direction, unit) 
-            end  
+                rotate_robot(direction, (unit % 4)) 
+            end
+            puts "#{$x} #{$y} #{$current_direction}"             
         end       
     end
+    min_distance = $x.abs() + $y.abs()
+    puts "\nMinimum distance to origin is #{min_distance}"
+    $x = 0
+    $y = 0
+    $current_direction = 0
 end
 
-def move_robot(direction, unit)
-    if current_direction == 0
-        if /^[#{FORWARD}]$/.match?(direction)
-            y += unit
+
+def move_robot(direction, unit)  
+    if $current_direction == 0
+        if /#{FORWARD}/.match?(direction)
+            $y += unit
         else
-            y -= unit
+            $y -= unit
         end
-    elsif current_direction == 1
-        if /^[#{FORWARD}]$/.match?(direction)
-            x += unit
+    elsif $current_direction == 1
+        if /#{FORWARD}/.match?(direction)
+            $x += unit
         else
-            x -= unit
+            $x -= unit
         end
-    elsif current_direction == 2
-        if /^[#{FORWARD}]$/.match?(direction)
-            y -= unit
+    elsif $current_direction == 2
+        if /#{FORWARD}/.match?(direction)
+            $y -= unit
         else
-            y += unit
+            $y += unit
         end
     else
-        if /^[#{FORWARD}]$/.match?(direction)
-            x -= unit
+        if /#{FORWARD}/.match?(direction)
+            $x -= unit
         else
-            x += unit
+            $x += unit
         end
     end              
 end
 
+
 def rotate_robot(direction, unit)
-    unit %= 4
     if unit == 1
-        if /^[#{RIGHT}]$/.match?(direction)
-            current_direction += 1
+        if /#{RIGHT}/.match?(direction)
+            $current_direction += 1
         else
-            current_direction += 3
+            $current_direction += 3
         end
     elsif unit == 2
-        current_direction += 2
+        $current_direction += 2
     elsif unit == 3
-        if /^[#{RIGHT}]$/.match?(direction)
-            current_direction += 3
+        if /#{RIGHT}/.match?(direction)
+            $current_direction += 3
         else
-            current_direction += 1
+            $current_direction += 1
         end
+    else
+        $current_direction += 0
     end
+    $current_direction %= 4
 end
 
 
 #Start point of the application
-puts "------------------------------------------------------------"
-puts "|\t\tWelcome to Robot Controller                |"
-puts "|\tYou can exit the application by typing `x`         |"
-puts "|\tYou can display instructions by typing `i`         |"
-puts "------------------------------------------------------------"
+puts "-----------------------------------------------------------"
+puts "|\t\tWelcome to Robot Controller               |"
+puts "|\tYou can exit the application by typing `x`        |"
+puts "|\tYou can display instructions by typing `i`        |"
+puts "-----------------------------------------------------------"
 instructions
 
 loop do
-    puts "Please enter your input"
+    puts "\nPlease enter your input"
     option = gets.chomp
     case option
     when "x"
+        puts "Thank you for your time. Have a good day!"
         break
     when "i"
         instructions
     else
         command_handler(option)
     end
-    break if option.downcase=="x"
+    break if option.downcase == "x"
 end
 
