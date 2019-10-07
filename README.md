6b39702242427838d5596e43b3b20eef647b5e90
Karthik <eyank1195@gmail.com>
2019-10-08 01:55:45 +1100

    Modified move_robot and rotate_robot methods to adopt to future extensions and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# CodeChallenge
- Code Challenge to find minimum distance to origin
+# Code Test - Robot Tracker
+## Ruby Application
+ Ruby program to track a robot, which receives simple input commands to move in a 2 Dimensional space. 
+ The goal of the application is to track the robot's movement and find the minimum distance it must travel to reach the origin
+
+ ### Available commands:
+* `F` - move forward 1 unit
+* `B` - move backward 1 unit
+* `R` - turn right 90 degrees
+* `L` - turn left 90 degrees
+
+### Sample Input and Output to test the app
+Input/Output Endpoint: - Console
+Inputs: - a string of comma-separated commands eg `F1,R1,B2,L1,B3`
+
+Output: - the minimum amount of distance to get back to the starting point (`4` in this case)
+
+### Instructions to install and execute the application
+#### Note: You might need latest ruby version, which can be found here https://rubyinstaller.org/downloads/
+* Clone the repository
+* Open Command prompt/Terminal and `cd` to the repository
+* Run  `ruby RobotTrackerApp.rb`
+
+### Design Assumptions and Decisions
+* The robot's movement can be tracked on a 2 Dimensional plan using x,y-axes and current direction using integers - 0,1,2,3
+* The initial displacement can be assigned to (0,0) [(x,y) coordinates]
+* The initial direction can be assigned 0(facing positive y-axis)
+* The program will only accept input in the form `<command><number>` (eg., `F,R`) will be invalidated
+
+
--- RobotTrackerApp.rb
+++ RobotTrackerApp.rb
@@ -1,8 +1,14 @@
-#Defining the constants for input commands
+
+# Defining the constants for input commands
+# Please define values in caps while extending/modifying in future
 FORWARD = "F"
 BACKWARD = "B"
 RIGHT = "R"
 LEFT = "L"
+
+# Defining all possible commands available for regular expression
+COMMANDS = "#{FORWARD}#{BACKWARD}#{RIGHT}#{LEFT}"
+MOVEMENT_COMMANDS = "#{FORWARD}#{BACKWARD}"
 
 =begin
 Setting the initial (x,y) coordinates of the Robot to (0,0)
@@ -12,118 +18,125 @@ $x = 0
 $y = 0
 $current_direction = 0
 
-#General instructions to users about the Robots Behaviour
-def instructions
-    puts "\n\t\t   <------Instructions------>"
-    puts "Control the robot's behaviour with a series of `<command><number>` pair"
-    puts "Use comma(,) to separate each pairs"
-    puts "The robot accepts the following commands:"
-    puts "\t* `#{FORWARD}` - move forward by 1 unit"
-    puts "\t* `#{BACKWARD}` - move backward by 1 unit"
-    puts "\t* `#{RIGHT}` - turn right by 90 degree"
-    puts "\t* `#{LEFT}` - turn left by 90 degree\n"
-    puts "The numbers specify the magnitude of the motion or rotation"
-    puts "For eg., '#{LEFT}2' means turn left by 180(2 times 90) degrees"
-    puts "\t '#{BACKWARD}3' means move backwards by 3 units"
-    puts "Sample Command: #{FORWARD}1,#{RIGHT}2,#{BACKWARD}2,#{LEFT}1,#{BACKWARD}3"
-end
-
-
-def command_handler(commands)  
-    list = commands.split(",")
-    list.each do |command|          
-        if !(/^([#{FORWARD}#{BACKWARD}#{RIGHT}#{LEFT}])([0-9]+)$/.match?(command.upcase)) 
-            puts "\nInvalid input command please check the instructions if you are unsure"
+=begin
+Method to handle the robot control command entered by user
+Validate the input:
+- If failed return to the main function
+- Else pass the control to respective function
+Once the functions are executed print the result
+=end
+def command_handler(input)  
+    list = input.split(",")
+    list.each do |instruction|   
+        instruction = instruction.upcase.gsub(/\s+/, "")
+        if !(/^[#{COMMANDS}]([0-9]+)$/.match?(instruction)) 
+            puts "\nInvalid input command. Please type `i` to refer to the instructions"
             return 
         else
-            direction = command[0].upcase   
-            unit = command[1, command.length-1].to_i  
-            if /^[#{FORWARD}#{BACKWARD}]$/.match?(direction)
-                move_robot(direction, unit)  
-            else
-                rotate_robot(direction, (unit % 4)) 
-            end           
+            command = instruction[0].upcase             
+            unit = instruction[1, instruction.length-1].to_i
+            /[#{MOVEMENT_COMMANDS}]/.match?(command) ? move_robot(command, unit) : rotate_robot(command, (unit % 4)) 
+            puts "#{$x} #{$y} #{$current_direction}"           
         end       
     end
-    min_distance = $x.abs() + $y.abs()
-    puts "\nMinimum distance to origin is #{min_distance}"
+    puts "\n[------] Minimum distance to origin is `#{$x.abs() + $y.abs()}`[------]"
     $x = 0
     $y = 0
     $current_direction = 0
 end
 
-
-def move_robot(direction, unit)  
-    if $current_direction == 0
-        if /#{FORWARD}/.match?(direction)
+# Method to check current_direction and move robot `unit` times
+def move_robot(command, unit)
+    if /#{FORWARD}/.match?(command)
+        case $current_direction 
+        when 0
             $y += unit
-        else
-            $y -= unit
-        end
-    elsif $current_direction == 1
-        if /#{FORWARD}/.match?(direction)
+        when 1
             $x += unit
+        when 2
+            $y -= unit
         else
             $x -= unit
         end
-    elsif $current_direction == 2
-        if /#{FORWARD}/.match?(direction)
+    elsif /#{BACKWARD}/.match?(command)
+        case $current_direction 
+        when 0
             $y -= unit
-        else
-            $y += unit
-        end
-    else
-        if /#{FORWARD}/.match?(direction)
+        when 1
             $x -= unit
+        when 2
+            $y += unit
         else
             $x += unit
-        end
-    end              
+        end            
+    end             
 end
 
-
-def rotate_robot(direction, unit)
-    if unit == 1
-        if /#{RIGHT}/.match?(direction)
+# Method to rotate the robot `unit` times and update the current_direction
+def rotate_robot(command, unit)
+    if /#{RIGHT}/.match?(command)
+        case unit
+        when 1
             $current_direction += 1
-        else
-            $current_direction += 3
-        end
-    elsif unit == 2
-        $current_direction += 2
-    elsif unit == 3
-        if /#{RIGHT}/.match?(direction)
+        when 2
+            $current_direction += 2
+        when 3
             $current_direction += 3
         else
-            $current_direction += 1
+            $current_direction += 0
         end
-    else
-        $current_direction += 0
+    elsif /#{LEFT}/.match?(command)
+        case unit
+        when 1
+            $current_direction += 3
+        when 2
+            $current_direction += 2
+        when 3
+            $current_direction += 1
+        else
+            $current_direction += 0
+        end
     end
     $current_direction %= 4
 end
 
+# General instructions to users about the commands to control Robot's Behaviour
+def instructions
+    puts "\n\t\t   <------Instructions------>"
+    puts "You can control the robot with a series of `<command><number>` pair"
+    puts "Use comma(,) to separate the pairs"
+    puts "The robot accepts the following commands:"
+    puts "\t* `#{FORWARD}` - move forward by 1 unit"
+    puts "\t* `#{BACKWARD}` - move backward by 1 unit"
+    puts "\t* `#{RIGHT}` - turn right by 90 degree"
+    puts "\t* `#{LEFT}` - turn left by 90 degree\n"
+    puts "The numbers specify the unit of motion or rotation"
+    puts "For eg., '#{LEFT}2' means turn left by 180(2 times 90) degrees"
+    puts "\t '#{BACKWARD}3' means move backwards by 3 units"
+    puts "Sample Command: #{FORWARD}1,#{RIGHT}1,#{BACKWARD}2,#{LEFT}1,#{BACKWARD}3"
+end
 
-#Start point of the application
-puts "-----------------------------------------------------------"
-puts "|\t\tWelcome to Robot Controller               |"
-puts "|\tYou can exit the application by typing `x`        |"
-puts "|\tYou can display instructions by typing `i`        |"
-puts "-----------------------------------------------------------"
+# Starting point of the application
+puts "\t-----------------------------------------------------------"
+puts "\t|\t\tWelcome to Robot Controller               |"
+puts "\t|\tYou can exit the application by entering `x`      |"
+puts "\t|\tYou can display instructions by entering `i`      |"
+puts "\t-----------------------------------------------------------"
 instructions
 
+# Get user input till the user wishes to exit
 loop do
     puts "\nPlease enter your input"
-    option = gets.chomp
-    case option
+    user_input = gets.chomp
+    case user_input
     when "x"
         puts "Thank you for your time. Have a good day!"
         break
     when "i"
         instructions
     else
-        command_handler(option)
+        command_handler(user_input)
     end
-    break if option.downcase == "x"
+    break if user_input.upcase == "X"
 end
 
